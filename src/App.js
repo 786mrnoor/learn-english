6620ddf84380a486208c2d1d751fc1341f717bcb
Noor Mohammad <786mrnoor@gmail.com>
2023-09-04 21:22:43 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicked, setClicked] = useState(0);
   return (
     <div className="App">
       <h1>This is Only Test Purpose</h1>
+      <h1>You Clicked {clicked}</h1>
+      <button onClick={() => setClicked(clicked + 1)}>Click Me</button>
     </div>
   );
 }
